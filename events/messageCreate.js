de6e44e9254b55f243f5aadc37ac2fe6e9262793
ShikoShintaro <shikoshintaro21@gmail.com>
2022-10-22 22:53:04 +0800

    updated Quetion event

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -4,39 +4,52 @@ const config = require("../config/shiko.json");
 const { Discord, MessageEmbed, Collection } = require("discord.js");
 const prefix = config.prefix
 
-client.on("messageCreate", async message => {
+
+client.on("messageCreate", async (message) => {
     if (message.author.bot) return;
-    if(!message.content.toLowerCase().startsWith(prefix.toLowerCase())) return;
-    const args = message.content.slice(prefix.length).split(/ +/);
+    if (!message.content.toLowerCase().startsWith(prefix.toLowerCase())) return;
+    const args = message.content.slice(prefix.length).trim().split(/ +/g)
     const commandName = args.shift().toLowerCase();
 
-    const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+    const command = client.commands.get(commandName) ||
+        client.commands.find
+            (cmd => cmd.aliases &&
+                cmd.aliases.includes(commandName));
 
     if (!command) return;
 
-    try{
+    try {
         await command.run(client, message, args);
     } catch (err) {
         console.error(err);
 
         message.reply('An error occurred!');
     }
-})
 
-client.on("messageCreate", (message) => {
-    if(message.content.startsWith("uwu")) {
-        client.subevents.get("olok").run(client, message)
-    }
-    else if (message.content.startsWith("cute")) {
-        client.subevents.get("kitty").run(client, message)
-    } 
-    else if (message.content.startsWith("Hi") && message.content.endsWith("Shiko")) {
-        client.subevents.get("res").run(client, message)
-    }
-    else if (message.content.startsWith("Lesson") && message.content.endsWith("Learned")) {
-        client.subevents.get("learned").run(client, message)
-    }
-    else if (message.content.startsWith("Lab u") && message.content.endsWith("Shiko")) {
-        client.subevents.get("labyo").run(client, message)
-    }
+    const msgArray = message.content.split(/\s+/g);
+    const args1 = msgArray.slice(1);
+
+
+
+
+
+    client.on("messageCreate", async (message) => {
+
+        if (message.content.startsWith("uwu")) {
+            client.subevents.get("olok").run(client, message, msgArray, args1)
+        }
+        else if (message.content.startsWith("cute")) {
+            client.subevents.get("kitty").run(client, message)
+        }
+        else if (message.content.startsWith("Shiko, ") && message.content.endsWith("?")) {
+            client.subevents.get("res").run(client, message, msgArray, args)
+        }
+        else if (message.content.startsWith("Lesson") && message.content.endsWith("Learned")) {
+            client.subevents.get("learned").run(client, message)
+        }
+        else if (message.content.startsWith("Lab u") && message.content.endsWith("Shiko")) {
+            client.subevents.get("labyo").run(client, message)
+        }
+
+    })
 })
--- events/subevents/res.js
+++ events/subevents/res.js
@@ -1,15 +1,49 @@
-module.exports.run = (client, message) => {
+const { EmbedBuilder } = require('discord.js')
+const ME = EmbedBuilder
+
+module.exports.run = (client, message, args) => {
     const Answer = [
-        "Hi",
-        "Hello",
+        "Yes",
+        "No",
+        "Certainly",
+        "Depends",
+        "HELL NO",
+        "I respectfully decline",
+        "Probably",
+        "Probably not",
+        "Cant think of anything",
+        "I disrespectfully decline",
+        "I Dont know sorry",
+        "Uhmmm.....No",
+        "I certainly don't know the answer...sorry.."
     ]
 
-    const randoms =  Math.max(1, Math.floor(Math.random() * Answer.length))
+    const randoms = Math.max(1, Math.floor(Math.random() * Answer.length))
 
-    message.reply(`${Answer[randoms]}, ${message.author}`)
+    const embed = new ME()
+        .addFields(
+            {
+                name: "Question : ",
+                value: `${message.content}`
+            }
+        )
+        .addFields(
+            {
+                name : "Answer : ",
+                value : `${Answer[randoms]}`
+            }
+        )
+        .setTimestamp()
+        .setColor("Random")
+        .setFooter({
+            text: `Request by ${message.author.tag}`,
+            iconURL: message.author.displayAvatarURL({ dynamic: true })
+          })
+
+    message.reply({ embeds : [embed] })
 }
 
 module.exports.config = {
     name: "res",
-    description: "Olok"
+    description: "Answers your question randomly"
 }
