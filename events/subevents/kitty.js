23ccd05d331b2f2fc4875bea22ad0d8fa2d7b5de
ShikoShintaro <shikoshintaro21@gmail.com>
2022-10-24 21:25:46 +0800

    s

--- events/subevents/kitty.js
+++ events/subevents/kitty.js
@@ -11,7 +11,6 @@ module.exports.run = (client, message) => {
         "https://tenor.com/view/nice-cat-gif-25643491",
         "https://tenor.com/view/cat-kitten-scare-scared-cat-scared-gif-24306961",
         "https://tenor.com/view/cat-cat-kiss-nelly-kiss-cute-cute-kitty-gif-24654570",
-        "https://cdn.discordapp.com/avatars/750208451287974000/ad4bcfa81abed9363dd89ebf04b14138.webp?size=4096"
     ]
 
     const rndmindx = Math.max(1, Math.floor(Math.random() * cute.length))
