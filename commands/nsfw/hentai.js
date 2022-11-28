f2698f3d8a7bbc951de56ce904d4eb377c94085c
Shiko Shintaro <shikoshintaro21@gmail.com>
2022-11-28 18:18:49 +0800

    NSFW Commands

---
+++ commands/nsfw/hentai.js
@@ -0,0 +1,28 @@
+const { EmbedBuilder } = require('discord.js');
+const neko = require('akaneko')
+
+module.exports = {
+    name: "hentai",
+    description: "NSFW FOR KIDS ONLY",
+    usage : ["s!hentai"],
+
+
+    run: async (client, message, args) => {
+
+        const err = new EmbedBuilder()
+            .setTitle("Oh No~")
+            .setDescription("This is not a nsfw channel sorry i cannot execute this command")
+            .setColor("Red")
+        if (!message.channel.nsfw)
+
+            return message.reply({ embeds: [err] })
+
+        const image = await neko.nsfw.hentai()
+
+        const embed = new EmbedBuilder()
+            .setImage(image)
+            .setColor("Random")
+        await message.reply({ embeds: [embed] })
+
+    }
+}
